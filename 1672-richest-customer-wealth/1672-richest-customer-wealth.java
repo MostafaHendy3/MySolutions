9309c059ab825d28d0b9087a0985782c28165cf7
MostafaHendy3 <88643633+MostafaHendy3@users.noreply.github.com>
2022-09-18 18:28:37 +0200

    Time: 4 ms (8.26%), Space: 43.2 MB (54.26%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -3,17 +3,12 @@ import java.util.Arrays;
 class Solution {
     public int maximumWealth(int[][] accounts) {
          int[] wealth = new int[accounts.length];
-            for(int i = 0 ; i<accounts.length;i++){
-                for(int j = 0;j<accounts[i].length;j++){
-                    wealth[i] += accounts[i][j];
-                }
+        for (int i = 0; i < accounts.length; i++) {
+            for (int j = 0; j < accounts[i].length; j++) {
+                wealth[i] += accounts[i][j];
             }
-            int max =wealth[0];
-            for (int i =1 ;i<wealth.length;i++){
-                if(wealth[i]>max){
-                    max = wealth[i];
-                }
-            }
-            return max;
+        }
+        
+        return Arrays.stream(wealth).max().getAsInt();
     }
 }
