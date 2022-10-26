08e49f234330d4ebb7458ee0642555df2831048f
MostafaHendy3 <88643633+MostafaHendy3@users.noreply.github.com>
2022-10-26 18:24:49 +0200

    Time: 30 ms (43.61%), Space: 49 MB (17.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        s=s.replaceAll("\\p{Punct}","");
+        s=s.replaceAll("\\p{Space}","");
+        if(s.length()==1)return true;
+
+        int i = 0;
+        int j = s.length()-1;
+        while(i<s.length()){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        
+        return true ;
+    }
+}
