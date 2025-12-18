da6fbc6060407bc7be43690ee078e41116a2fdf1
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2025-12-18 16:23:55 +0200

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,17 +1,14 @@
 1class Solution {
-2    public int[] twoSum(int[] nums, int target) {
-3     int[] res ={0,0};
-4        outerloop:
-5        for(int i=0;i<nums.length;i++){
-6            res[0]=i;
-7            for(int j=i+1;j<nums.length;j++){
-8                if(target == nums[i]+nums[j]){
-9                    res[1]=j; 
-10                    break outerloop;
-11                }
-12            }
-13            
-14        }
-15        return res;    
-16    }
-17}
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4         for (int i = 0; i < nums.size(); i++) {
+5            for (int j = i + 1; j < nums.size(); j++) {
+6                if (nums[j] == target - nums[i]) {
+7                    return {i, j};
+8                }
+9            }
+10        }
+11        // Return an empty vector if no solution is found
+12        return {};
+13    }
+14};
