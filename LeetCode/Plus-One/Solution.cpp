c72d9ac5018c346970d8b52a96dab53f5336274b
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2025-12-18 14:57:33 +0200

    Add solution for Plus One

---
+++ LeetCode/Plus-One/Solution.cpp
@@ -0,0 +1,14 @@
+1class Solution {
+2public:
+3    vector<int> plusOne(vector<int>& digits) {
+4        for (int i = digits.size() - 1; i >= 0; i--) {
+5            if (digits[i] < 9) {
+6                digits[i]++;
+7                return digits;
+8            }
+9            digits[i] = 0;
+10        }
+11        digits.insert(digits.begin(), 1);
+12        return digits;
+13    }
+14};
