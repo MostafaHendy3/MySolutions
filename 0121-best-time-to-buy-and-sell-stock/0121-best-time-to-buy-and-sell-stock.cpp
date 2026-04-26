c75d69522140f3f415bb7a5019289be18a569568
Mostafa Hendy <88643633+MostafaHendy3@users.noreply.github.com>
2026-04-27 00:20:44 +0300

    Time: 0 ms (100%), Space: 97.4 MB (59.83%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int right = 0 ;
+        int left = 0;
+        int maxProfit = 0;
+        while (right < prices.size()) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            }else{
+                maxProfit = max(maxProfit, prices[right] - prices[left]);
+                right++;
+            }
+        }
+        return maxProfit;
+    }
+};
